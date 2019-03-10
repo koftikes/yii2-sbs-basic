540f2149cc2795baa02ecc24d905927c39a811da
Konstantin Litvinov <koftikes@gmail.com>
2019-03-10 16:35:32 +0300

    Added install tool, added local configs, entry points and environments folder.

--- config/test.php
+++ config/test.php
@@ -1,42 +1,28 @@
 <?php
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/test_db.php';
 
 /**
  * Application configuration shared by all test types
  */
-return [
-    'id' => 'basic-tests',
-    'basePath' => dirname(__DIR__),
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
-    'language' => 'en-US',
-    'components' => [
-        'db' => $db,
-        'mailer' => [
-            'useFileTransport' => true,
-        ],
-        'assetManager' => [
-            'basePath' => __DIR__ . '/../web/assets',
-        ],
-        'urlManager' => [
-            'showScriptName' => true,
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-        ],
-        'request' => [
-            'cookieValidationKey' => 'test',
-            'enableCsrfValidation' => false,
-            // but if you absolutely need it set cookie domain to localhost
-            /*
-            'csrfCookie' => [
-                'domain' => 'localhost',
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/main-local.php',
+    [
+        'id' => 'basic-tests',
+        'components' => [
+            'assetManager' => [
+                'basePath' => __DIR__ . '/../web/assets',
             ],
-            */
+            'user' => [
+                'identityClass' => 'app\models\User',
+            ],
+            'urlManager' => [
+                'showScriptName' => true,
+            ],
+            'request' => [
+                'enableCsrfValidation' => false,
+                // but if you absolutely need it set cookie domain to localhost uncomment this
+                // 'csrfCookie' => ['domain' => 'localhost'],
+            ],
         ],
-    ],
-    'params' => $params,
-];
+    ]
+);
